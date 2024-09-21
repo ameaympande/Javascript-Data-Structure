3a72f6388c2d97f6d616aa7a90e43307f3000efc
ameay <ameay35@gmail.com>
2024-09-21 22:44:01 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, "Ameay"];
+
+arr.push(4);
+arr.unshift(0);
+
+arr.pop();
+arr.shift();
+for (item of arr) {
+  console.log(item);
+}
+
+// map , filter , reduce , concat slice and splice
---
+++ linkList.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const list = new LinkedList();
+
+console.log("Is list is empty ? :", list.isEmpty());
+console.log("Size of the list : ", list.getSize());
